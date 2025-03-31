1761f9437055a5b2666eafdf30439b4b8c824467
Nivedika-sharma <sharmanivedika6@gmail.com>
2025-03-31 17:45:36 +0530

    video added

--- src/components/GridBackground.tsx
+++ src/components/GridBackground.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import FloatingShape from "./FloatingShape";
+import video from "../assets/jam-analytics-video.mp4";
 
 const GridBackground = () => {
   return (
@@ -23,26 +24,33 @@ const GridBackground = () => {
         <FloatingShape color="bg-purple-900" size="w-36 h-36" top="70%" left="75%" />
 
         {/* Text Section */}
-        <div className="absolute top-20 left-1/2 transform -translate-x-1/2 w-[90%]  text-center">
+        <div className="absolute  left-1/2 transform -translate-x-1/2 w-[90%]  text-center top-12">
           <h1
             className="text-white text-4xl font-bold italic"
             style={{ fontFamily: "Bona Nova, italic" }}
           >
             You’ve Been Doing It the Hard Way… It’s Time to Change That!
           </h1>
-          <p className="text-gray-500 text-lg mt-4 max-w-2xl mx-auto leading-tight">
+          <p className="text-gray-500 text-lg mt-3 max-w-2xl mx-auto leading-tight">
             Juggling between multiple software? <br />
             Using spreadsheets & paper for critical processes? <br />
             No insights from sales calls? <br />
-            No insights from sales calls?
+            What if AI could handle all of this—so you can focus on growing?
           </p>
 
 
 
-          
-          {/* jam-analytic-video */}
-          <div>
 
+          {/* jam-analytic-video */}
+          <div className ="relative items-center justify-center left-[23rem]  w-[46%] h-[60%] pt-6">
+            <div className="relative rounded-xl shadow-[20px_20px_24px_#0000008c]  before:absolute before:top-[50%] before:left-[50%] before:bottom-[50%] before:right-[50%] before:bg-[conic-gradient(transparent,transparent,)]before:animate-spin-slow">
+
+            <div>  
+            <video src={video} autoPlay loop muted className="w-auto h-auto mt-8 rounded-xl shadow-lg" />
+            </div>
+            </div>
+            
+              
           </div>
         </div>
       </div>
