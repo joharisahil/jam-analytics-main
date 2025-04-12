fc1a5126dbacde37638d3a60ff23f86843b47865
Nivedika-sharma <sharmanivedika6@gmail.com>
2025-04-12 19:07:31 +0530

    video sizing and spacing

--- src/components/GridBackground.tsx
+++ src/components/GridBackground.tsx
@@ -50,21 +50,21 @@ const GridBackground = () => {
         {/* Text Section */}
         <div className="absolute left-1/2 transform -translate-x-1/2 w-[90%] text-center top-20">
           <h1
-            className="text-white text-3xl sm:text-4xl md:text-5xl lg:text-[60px] xl:text-[75px] font-medium leading-none"
+            className="text-white text-2xl sm:text-3xl md:text-4xl lg:text-[60px] xl:text-[75px] font-medium leading-none"
             style={{ fontFamily: "Castoro" }}
           >
-            Ask Anything Related To Your Job
+            Automate Your Job
           </h1>
 
           <div className="relative flex items-center justify-center w-[80%] sm:w-[60%] md:w-[60%] lg:w-[46%] h-[100%] pt-24 mx-auto transition-transform duration-300 ease-in-out hover:scale-125">
       {/* "Jam AI" Logo Div */}
-      <div className="absolute top-12 left-2 flex items-center space-x-2 px-2 py-1 border border-white/50 text-white bg-transparent rounded-3xl">
+      <div className="absolute top-12 mt-4 left-2 flex items-center space-x-2 px-2 py-1 border border-white/50 text-white bg-transparent rounded-3xl">
         <img
           src={image}
           alt="jam-analytic-logo"
           className="rounded-full w-5 h-5 border-2 border-white"
         />
-        <p className="font-inter text-white text-sm sm:text-base md:text-lg">
+        <p className="font-inter  text-white text-sm sm:text-base md:text-lg">
           Jam AI
         </p>
       </div>
@@ -74,7 +74,7 @@ const GridBackground = () => {
         initial={{ scale: 1.25, opacity: 0 }}
         animate={{ scale: 1, opacity: 1 }}
         transition={{ duration: 1.2, ease: "easeOut" }}
-        className="w-full rounded-[20px] overflow-hidden shadow-lg"
+        className="w-full mt-6 rounded-[20px] overflow-hidden shadow-lg"
       >
         <video
     src={videoDesktop}
