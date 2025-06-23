2ddf7d805de54eb90bcc5da3087da670b50df4fa
Nivedika-sharma <sharmanivedika6@gmail.com>
2025-06-23 11:25:51 +0530

    otp text fix

--- src/components/Otp.tsx
+++ src/components/Otp.tsx
@@ -96,7 +96,7 @@ const OtpVerification: React.FC = () => {
             Enter 6 digit code
           </h2>
           <p className="text-sm text-gray-500 mt-1">
-            A four-digit code should have come to your email address that you indicated.
+            A six-digit code should have come to your phone number that you indicated.
           </p>
         </div>
 
